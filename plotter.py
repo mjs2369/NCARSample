51cea56b719c7f11a0375e945b8e5d5f9dad6a1c
Marlena Smith <44214771+mjs2369@users.noreply.github.com>
2022-03-22 15:48:17 -0400

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -50,7 +50,8 @@ def plot_bestfit(self, nbins=10, phase=True, title=""):
     ax_lc.get_xaxis().set_visible(False)
 
    ## binner() is a method previously defined in EXOTIC - condenses 10 pixels into 1
-   ## reduces the runtime of imaging processing, reduces the statistical significange of error values 
+   ## reduces the runtime of imaging processing 
+   ## increases signal to noise ratio at each pixel by reducing the statistical significance of error values 
     ax_res.errorbar(binner(ecks, len(self.residuals) // 10),
                     binner(self.residuals / np.median(self.data), len(self.residuals) // 10),
                     yerr=
